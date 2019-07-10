d66d41af03976bc118072e553e35223bf1c2df4c
arthi <artowuday@gmail.com>
2019-07-10 15:57:45 -0700

    last of the day

--- Languages/my-lang/examples.rkt
+++ Languages/my-lang/examples.rkt
@@ -42,3 +42,26 @@
 
   )
 
+;Create a solid blue, star-shaped polygon, with step count of 2.
+;Hint: side-length, point count, step count, fill, color.
+(define-exampe-code-1 racket nish
+(star-polygon 40 5 2 "solid" "blue"))
+
+;Create a regular 6-sided polygon (hexagon) with red outline.
+;Hint: side-length, side count, fill, color.
+(regular-polygon 40 6 "outline" "red")
+
+;Create a Regular Polygon with 7 sides, that has one-third pull
+;on each side, acute angle, and a purple outline.
+;Hint: side-length, side-count, pull (the curve or pulling of the sides),
+;angle, fill, color.
+(pulled-regular-polygon 50 7 1/3 30 "outline" "purple")
+
+;Create a solid green polygon with a list of 4 points that create an arrow shape.
+;**Must require lang/posn in order to "make-posn"
+;Hint: list of vertices, fill, color
+(polygon(list(make-posn 0 0)
+               (make-posn -10 20)
+               (make-posn 60 0)
+               (make-posn -10 -20))
+               "solid" "green")
