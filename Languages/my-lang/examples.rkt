65d2d4a979bd7c94cf83b209f61bab70cf96e3f6
arthi <artowuday@gmail.com>
2019-07-11 12:01:16 -0700

    Michael adding katas

--- Languages/my-lang/examples.rkt
+++ Languages/my-lang/examples.rkt
@@ -34,6 +34,35 @@
 
 ; ============ NEW KATAS 07/08/19 ===============
 
+;Create a solid blue, star-shaped polygon, with step count of 2.  
+;Hint: side-length, point count, step count, fill, color.
+
+(define-example-code polygon michael-nish-polygon-001
+  
+  (star-polygon 40 5 2 "solid" "blue"))
+
+;Code a regular 6-sided polygon (hexgaon) with red fill.
+;Hint: side-length, side count, fill, color.
+(define-example-code polygon michael-nish-polygon-002
+  
+  (regular-polygon 40 6 "outline" "red"))
+
+;Create a Pulled Polygon with 7 sides, that has one-third pull on each side, and a purple outline.  
+;Hint: side-length, side-count, pull (the curve or pulling of the sides), angle, fill, color.
+(define-example-code polygon michael-nish-polygon-003
+  
+(pulled-regular-polygon 70 7 1/3 30 "outline" "purple"))
+
+;Create a solid green, arrow-shaped polygon with 4 vertices.
+;Hint: list of vertices, fill, color
+(define-example-code polygon michael-nish-polygon-004
+  
+ (polygon (list (make-posn 0 0)
+               (make-posn -10 20)
+               (make-posn 60 0)
+               (make-posn -10 -20))
+               "solid" "green"))
+
 ; map a function to a list of 10 numbers,
 ; 0 through 9, that reduces each value by 1
 (define-example-code racket jason-easy-map-001
--- scribblings/new-katas/new-katas-070819.scrbl
+++ scribblings/new-katas/new-katas-070819.scrbl
@@ -9,3 +9,4 @@
 @section{intro}
 
 @(render jason)
+@(render michael-nish)
