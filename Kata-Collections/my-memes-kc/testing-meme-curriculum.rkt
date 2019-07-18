6f53951710e229fc2ba0d5afa5c20ebd82920df2
tnbarton <tommynbar@gmail.com>
2019-07-18 14:48:46 -0700

    Added one more meme example

--- Kata-Collections/my-memes-kc/testing-meme-curriculum.rkt
+++ Kata-Collections/my-memes-kc/testing-meme-curriculum.rkt
@@ -5,3 +5,11 @@
 (define text2 (text "Working as a camp counselor \nfor coding knowledge" 36 "black"))
 
 (overlay/align "right" "top" text1 (overlay/align "right" "middle" text2 image))
+
+
+(define headache (bitmap "./assets/Types-of-Headaches.png"))
+(define text3 (text/font "WhEn ThEy d0n'T\ncLoSe tHe pArEnThEsEs" 25 "black"
+                         "Gill Sans" 'swiss 'normal 'bold #f))
+
+(overlay/offset text3 -120 -120 headache)
+
