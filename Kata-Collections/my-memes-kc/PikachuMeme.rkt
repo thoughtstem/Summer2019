d4eb8646dfbeb71f17979bb7b11ac61bbb4ac933
DrewKeirn <drewkeirn@gmail.com>
2019-07-19 13:52:27 -0700

    Made a dank surprised pikachu face meta  meme

---
+++ Kata-Collections/my-memes-kc/PikachuMeme.rkt
@@ -0,0 +1,17 @@
+#lang fundamentals
+
+(require 2htdp/universe
+         2htdp/image)
+
+(define image (bitmap "assets/Surprised-Pikachu.png"))
+
+
+(define words (text "Student: *applies for unpaid internship
+Student: *is unpaid*
+Student:" 36 'black))
+
+
+(overlay/offset 
+ words
+ 10 120
+ image)
