169866f4146aa711a1061e4a7b3722a69b9d1f12
ezratkim <ezra.t.kim@gmail.com>
2019-07-22 15:45:54 -0700

    Added examples.rkt for game

---
+++ Languages/my-game-lang/examples.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require ts-kata-util)
+
+
+;Kata 1: Make an eye from an outlined circle with a smaller solid black circle
+;inside of it (the pupil). Make the eyes rotate counterclockwise over time.
+
+(define-example-code Summer2019/Languages/my-game-katas Rotating-Eye
+  ((require 2htdp/universe 2htdp/image)
+
+; TO-DRAW
+(define (create-spinning-eye angle)
+  (rotate angle eye))
+
+; The eye sprite
+(define eye (overlay/offset (circle 12 "solid" "black") 0 18
+                    (circle 30 "outline" "black"))))
+
+(big-bang 0
+  (on-tick add1)
+  (to-draw create-spinning-eye)))
