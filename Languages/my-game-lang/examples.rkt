33e3b2047bba4d8044268131c05af516f7ab6803
Titan-Ngo <ttngo@ucsd.edu>
2019-07-23 13:53:58 -0700

    Updated game katas to be working

--- Languages/my-game-lang/examples.rkt
+++ Languages/my-game-lang/examples.rkt
@@ -6,17 +6,18 @@
 ;Kata 1: Make an eye from an outlined circle with a smaller solid black circle
 ;inside of it (the pupil). Make the eyes rotate counterclockwise over time.
 
-(define-example-code Summer2019/Languages/my-game-lang Rotating-Eye
-  ((require 2htdp/universe 2htdp/image)
+(define-example-code Summer2019/Languages/my-game-lang/main rotating-eye
 
 ; TO-DRAW
-(define (create-spinning-eye angle)
+(define (create-rotating-eye angle)
   (rotate angle eye))
 
 ; The eye sprite
 (define eye (overlay/offset (circle 12 "solid" "black") 0 18
-                    (circle 30 "outline" "black"))))
+		    (circle 30 "outline" "black")))
 
 (big-bang 0
   (on-tick add1)
-  (to-draw create-spinning-eye)))
+  (to-draw create-rotating-eye)))
+
+
