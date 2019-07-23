d3270b1b6c7d99a9144a42b79a37f03a3f10b79a
--global <--global>
2019-07-23 15:08:33 -0700

    jame's blog and some other fixes

--- Languages/my-game-lang/examples.rkt
+++ Languages/my-game-lang/examples.rkt
@@ -6,17 +6,15 @@
 ;Kata 1: Make an eye from an outlined circle with a smaller solid black circle
 ;inside of it (the pupil). Make the eyes rotate counterclockwise over time.
 
-(define-example-code Summer2019/Languages/my-game-lang Rotating-Eye
-  ((require 2htdp/universe 2htdp/image)
+(define-example-code Summer2019/Languages/my-game-lang/main Rotating-Eye
+		     ; TO-DRAW
+		     (define (create-spinning-eye angle)
+		       (rotate angle eye))
 
-; TO-DRAW
-(define (create-spinning-eye angle)
-  (rotate angle eye))
+		     ; The eye sprite
+		     (define eye (overlay/offset (circle 12 "solid" "black") 0 18
+						 (circle 30 "outline" "black")))
 
-; The eye sprite
-(define eye (overlay/offset (circle 12 "solid" "black") 0 18
-                    (circle 30 "outline" "black"))))
-
-(big-bang 0
-  (on-tick add1)
-  (to-draw create-spinning-eye)))
+		     (big-bang 0
+			       (on-tick add1)
+			       (to-draw create-spinning-eye)))
--- scribblings/team-profiles/chris-harper.scrbl
+++ scribblings/team-profiles/chris-harper.scrbl
@@ -6,11 +6,7 @@
           "../new-tactics/bribe-tactic.rkt")
 
 @(bio "Christopher Harper"
-<<<<<<< HEAD
       "tree.png"
-=======
-      "chris-harper.png"
->>>>>>> e1992b567a435fb33300d2db2450adfc18d5ac07
       "Hi my name is Christopher Harper")
       
-  @blog{ }
+@blog{ }
