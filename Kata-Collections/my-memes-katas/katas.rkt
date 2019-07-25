5d044c814a923998e8b3b425fbbcf1d19a3a1f25
slpeeler <slpeeler@berkeley.edu>
2019-07-25 15:04:58 -0700

    formatted meme katas. webpage comes up correctly

--- Kata-Collections/my-memes-katas/katas.rkt
+++ Kata-Collections/my-memes-katas/katas.rkt
@@ -6,11 +6,22 @@
 
 (define katas
   (fill-in-stimuli
-    (lang->kata-collection 'Summer2019/Languages/my-fundamentals-lang)
+    (lang->kata-collection 'Summer2019/Languages/my-memes-lang)
 
-    ;To add a new kata, add two new lines of code in this block
-    ; 1) 'the-name-of-your-kata
-    ; 2) (read "your kata's stimuli. ex: "Create a circle...")
+    '001-button-meme
+    (read "Make a button meme. Put text over the button, as well as text over the moving hand.")
+
+    '002-distracted-boyfriend-meme
+    (read "Make a distracted boyfriend meme. Put text over the girlfriend, the boyfriend, and the new girl")
+
+    '003-headache-meme
+    (read "Make a Headache meme. Add text below the image of the full head headache.")
+
+    '004-pikachu-meme
+    (read "Make a Pikachu meme. Add text to the white space above the meme image.")
+
+    '005-pooh-meme
+    (read "Make a Pooh meme. Add text in the white space next to each Pooh image in the meme.")
 
     ))
 
