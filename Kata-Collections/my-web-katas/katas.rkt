410ad07d85b1c8ca17d538b1294f72026fdff482
Dietrich <rahulinsd@gmail.com>
2019-07-25 13:37:46 -0700

    fixed my-web-katas build error

--- Kata-Collections/my-web-katas/katas.rkt
+++ Kata-Collections/my-web-katas/katas.rkt
@@ -6,30 +6,14 @@
 
 (define katas
   (fill-in-stimuli
-    (lang->kata-collection 'Summer2019/Languages/my-fundamentals-lang)
-    'superset-lang-demo
-    (read "This is a demo kata to show you how to make katas with a superset lang and a stimulus.")
-    ))
+    (lang->kata-collection 'Summer2019/Languages/my-web-lang)))
 
-(define-sub-collections katas
-  superset-lang)
+(define-sub-collections katas)
 
 (define-sub-collections katas
                         easy
                         medium
                         hard
                         ; ==== ADD MORE SUB COLLECTIONS HERE ====
-                        jason
-
-                        michael-nish
-                        cristine-sandora 
-                        jacob-dietrich
-                        ambre-emily
-                        jake-jack
-                        tom-maya
-                              
-                        grpa
-                        katas-b
-                        grpC
   )
 
