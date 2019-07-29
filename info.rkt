486efebc94e96ce83e63b2513173b9760021d6ff
DrewKeirn <keirna@purdue.edu>
2019-07-29 13:33:23 -0700

    fixed build

--- info.rkt
+++ info.rkt
@@ -10,9 +10,6 @@
                       ("scribblings/new-katas/new-katas-b.scrbl" ())  
                       ("scribblings/new-katas/new-katas-c.scrbl" ())
 
-                      ("scribblings/new-katas/new-katas-070819.scrbl" ()) 
-                      
-
                       ("scribblings/tactics-variants/tactics-variants-a.scrbl" ())  
                       ("scribblings/tactics-variants/tactics-variants-b.scrbl" ())))
 (define pkg-desc "Description Here")
