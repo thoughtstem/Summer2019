1670f76db4fdf6838b5c497aaeaac08cda918eb6
owen <zhpi@ucsd.edu>
2019-07-30 13:08:46 -0700

    Updated katas for my-web-katas

--- Languages/my-web-lang/examples.rkt
+++ Languages/my-web-lang/examples.rkt
@@ -7,7 +7,7 @@
 
 ; Easy: Create a blog with a title.
 
-(define-example-code Summer2019/Languages/my-web-lang/main 001-blog-with-title
+(define-example-code Summer2019/Languages/my-web-lang/main 001-blog-title
  
   (define (start request)
     (response/xexpr
@@ -20,7 +20,7 @@
 
 ; Easy: Create a blog with a title and a post.
 
-(define-example-code Summer2019/Languages/my-web-lang/main 002-blog-with-a-post
+(define-example-code Summer2019/Languages/my-web-lang/main 002-blog-post
 
   (define (start request)
     (response/xexpr
@@ -35,7 +35,7 @@
 
 ; Medium: Create a blog with a title and multiple posts.
 
-(define-example-code Summer2019/Languages/my-web-lang/main 003-blog-with-many-posts
+(define-example-code Summer2019/Languages/my-web-lang/main 003-multiple-blog-posts
 
   (define (start request)
     (response/xexpr
@@ -54,7 +54,7 @@
 
 ; Medium: Create a blog with a title and multiple posts and links to the the ThoughtSTEM website in one of the posts.
 
-(define-example-code Summer2019/Languages/my-web-lang/main 004-blog-with-many-posts-and-links
+(define-example-code Summer2019/Languages/my-web-lang/main 004-links
 
   (define (start request)
     (response/xexpr
@@ -64,17 +64,54 @@
        (a ((href "https://www.thoughtstem.com/")) "About Us")
        (body (h1 "Welcome to My Webpage!"))
        (define (list (h3 "My First Blog Post!" )
-                     (post "Hey, this is my first post!")))
-       (ul
-        (li (a ((href "Link.html")) "Intro"))
-        (li (a ((href "Link.html")) "Outro"))))))
+                     (post "Hey, this is my first post!"))))))
   
   (serve/servlet start)
   )
 
+; Medium: Create a blog with an ordered-list of links
+
+(define-example-code Summer2019/Languages/my-web-lang/main 005-ordered-list
+
+(define (start request)
+  (response/xexpr
+   '(html
+     '(style (head {position: absolute top: 100px}))
+     (head (title "My Blog"))
+     (a ((href "https://www.thoughtstem.com/")) "About Us")
+     (body (h1 "Welcome to My Webpage!"))
+     (define (list (h3 "My First Blog Post!" )
+                   (post "Hey, this is my first post!")))
+     (ol
+      (li (a ((href "https://en.wikipedia.org/wiki/First")) "First"))
+      (li (a ((href "https://en.wikipedia.org/wiki/Second")) "Second"))))))
+  
+(serve/servlet start)
+)
+
+; Medium: Create a blog with an unordered-list of links
+
+(define-example-code Summer2019/Languages/my-web-lang/main 006-unordered-list
+
+(define (start request)
+  (response/xexpr
+   '(html
+     '(style (head {position: absolute top: 100px}))
+     (head (title "My Blog"))
+     (a ((href "https://www.thoughtstem.com/")) "About Us")
+     (body (h1 "Welcome to My Webpage!"))
+     (define (list (h3 "My First Blog Post!" )
+                   (post "Hey, this is my first post!")))
+     (ul
+      (li (a ((href "https://en.wikipedia.org/wiki/Begin")) "Begin"))
+      (li (a ((href "https://en.wikipedia.org/wiki/End")) "End"))))))
+  
+(serve/servlet start)
+)
+
 ; Hard: Create a blog and add a colorful title with colorful posts using basic CSS.
 
-(define-example-code Summer2019/Languages/my-web-lang/main 005-blog-with-a-colorful-title
+(define-example-code Summer2019/Languages/my-web-lang/main 007-colorful-title
 
   (define (start request)
     (response/xexpr
@@ -91,7 +128,7 @@
 
 ; Hard: Create a blog with an image
 
-(define-example-code Summer2019/Languages/my-web-lang/main 006-blog-with-an-image
+(define-example-code Summer2019/Languages/my-web-lang/main 008-images
   (define (start request)
     (response/xexpr
      '(html
