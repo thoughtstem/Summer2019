6278d088c38ac8dbf0420a7ac11b4b3544e82d53
owen <zhpi@ucsd.edu>
2019-07-31 13:40:49 -0700

    Added more web katas

--- Kata-Collections/my-web-katas/katas.rkt
+++ Kata-Collections/my-web-katas/katas.rkt
@@ -26,13 +26,19 @@
     '006-unordered-list
     (read "Create a webpage with a title, a blog post and an unordered list of links.")
 
-    '007-colorful-title
-    (read "Create a webpage with a different colored title and a blog post.")
+    '007-text-color
+    (read "Create a webpage with a blog and a colorful title.")
 
-    '008-font-formatting
+    '008-text-outline
+    (read "Create a webpage with a blog and a colorful outline for the text.")
+
+    '009-text-shadow
+    (read "Create a webpage with a blog and add shadows attached to your text.")
+
+    '010-font-formatting
     (read "Create a blog and add a colorful title with colorful posts using basic CSS and change all aspects of the font.")
 
-    '009-images
+    '011-images
     (read "Create a webpage with a title and a blog post containing an image.")
     ))
 
