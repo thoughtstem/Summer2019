1d18846337dacf4363318ece6a3ef536091960ea
tnbarton <tommynbar@gmail.com>
2019-07-31 13:16:26 -0700

    Added a text formatting kata to web dev

--- Languages/my-web-lang/examples.rkt
+++ Languages/my-web-lang/examples.rkt
@@ -127,9 +127,35 @@
   (serve/servlet start)
   )
 
+; Hard: Create a blog and add a colorful title with colorful posts using basic CSS and change all aspects of the font.
+
+(define-example-code Summer2019/Languages/my-web-lang/main 008-font-formatting
+
+  (define (start request)
+    (response/xexpr
+     '(html
+       '(style
+         ((type "text/css"))
+         "h1 { color: green; font-family: serif; font-style: normal }"
+          "body { color: aquamarine; font-family: serif; font-style: italic; font-size: 44px; text-align: center}"
+"h2 { color: magenta; font-family: arial; font-style: normal}"
+"h3 { color: yellow; font-family: arial; font-style: normal; font-weight: bold"
+         )
+       (head (title "My Blog"))
+       (body (h1 "Welcome to My Webpage!"))
+       (define (list
+                (h2
+                 "My Magenta-colored Blog Post!")
+                (post
+                 "Hey, this is my aquamarine-colored post!")
+                (h3 "Hope you liked it!"))))))
+ 
+  (serve/servlet start)
+  )
+
 ; Hard: Create a blog with an image
 
-(define-example-code Summer2019/Languages/my-web-lang/main 008-images
+(define-example-code Summer2019/Languages/my-web-lang/main 009-images
   (define (start request)
     (response/xexpr
      '(html
--- Languages/my-web-lang/testing/testing.rkt
+++ Languages/my-web-lang/testing/testing.rkt
@@ -1,23 +1,15 @@
 #lang Summer2019/Languages/my-web-lang/main
- 
+ #|#
 (define (start request)
   (response/xexpr
    '(html
-     '(style
-       ((type "text/css"))
-       "h1 { color: green; font-family: serif; font-style: normal }
-       body { color: aquamarine; font-family: serif;
-font-style: italic; font-size: 44px; text-align: center}
-       h2 { color: magenta; font-family: arial; font-style: normal}
-h3 { color: yellow; font-family: arial; font-style: normal; font-weight: bold"
-       )
      (head (title "My Blog"))
      (body (h1 "Welcome to My Webpage!"))
      (define (list
               (h2
-               "My Magenta-colored Blog Post!")
-              (post
-               "Hey, this is my aquamarine-colored post!")
-              (h3 "Hope you liked it!"))))))
+               "Check out this cool video!")
+       )))))
  
-(serve/servlet start)
+(serve/servlet start)|#
+
+#|#https://www.youtube.com/watch?v=eKqPCBbaZ7E|#
