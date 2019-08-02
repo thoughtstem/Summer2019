dd779b4834e45c1aa4e000e7c849e1b895a5db8f
Niharika Malhotra <mail2niharikam@gmail.com>
2019-08-02 12:09:32 -0700

    Changes

--- Languages/my-memes-lang/lang/main.rkt
+++ Languages/my-memes-lang/lang/main.rkt
@@ -8,4 +8,32 @@
          "../assets.rkt"
          2htdp/image ;You'll probably delete this.
          ;If skinning a language, require that here
-	 )
+         )
+
+(define drake (bitmap "../assets/Drake.png"))
+
+(define confused-anime
+   (bitmap "../assets/Confused-Anime.png"))
+
+(define pikachu
+   (bitmap "../assets/Surprised-Pikachu.png"))
+
+(define headache
+   (bitmap "../assets/Types-of-Headaches.png"))
+
+(define pooh
+  (bitmap "../assets/Pooh.png"))
+
+(define button (bitmap "../assets/Button.png"))
+
+(define distracted-boyfriend
+   (bitmap "../assets/Distracted-Boyfriend.png"))
+
+
+(provide drake)
+(provide confused-anime)
+(provide pikachu)
+(provide headache)
+(provide pooh)
+(provide button)
+(provide distracted-boyfriend)
