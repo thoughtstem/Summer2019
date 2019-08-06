7d7175a1c146c3fd56441059d2519089d7da4bdb
Jack R <rossjack777@gmail.com>
2019-08-06 13:53:59 -0700

    edits to fractals docs

---
+++ Kata-Collections/my-fractal-katas/katas.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(provide katas)
+
+(require ts-kata-util/katas/main)
+
+(define katas
+  (fill-in-stimuli
+    (lang->kata-collection 'Summer2019/Languages/my-fractals-lang)
+
+    ;Fill in stimuli and examples in this format:
+    ;'001-easy
+    ;(read "Put stimuli here.")
+
+
+(define-sub-collections katas
+                        easy
+                        medium
+                        hard
+                        
+  )
+
