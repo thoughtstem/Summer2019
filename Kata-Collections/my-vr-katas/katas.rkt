ed01130213703aa2633322301283ba6ee2847371
Manda <mandatran@roadrunner.com>
2019-08-16 15:22:50 -0700

    added vr examples

--- Kata-Collections/my-vr-katas/katas.rkt
+++ Kata-Collections/my-vr-katas/katas.rkt
@@ -17,6 +17,18 @@
    
     'shapes~box-001
     (read "Write a function that produces a blue box on a black background")
+
+    'shapes~snowman-001
+    (read "Create a scene with a white sphere on a light blue background.")
+
+    'shapes~snowman-002
+    (read "Create a scene with a snowman on a light blue background.")
+
+    'shapes~snowman-003
+    (read "Create a scene with a snowman and tree on a light blue background.")
+
+    'shapes~snowman-004
+    (read "Create a scene with a snowman and tree on a light blue background with snow.")
   
   
    ))
